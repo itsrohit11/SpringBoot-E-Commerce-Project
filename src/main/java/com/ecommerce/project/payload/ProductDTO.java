4d10345b93874a01a9e9b701b1286ffa3b224a5e
itsrohit11 <officialrohitjain11@gmail.com>
2025-02-13 21:38:37 +0530

    Product Info Adding

--- src/main/java/com/ecommerce/project/payload/ProductDTO.java
+++ src/main/java/com/ecommerce/project/payload/ProductDTO.java
@@ -1,28 +1,20 @@
-package com.ecommerce.project.model;
+package com.ecommerce.project.payload;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+
+public class ProductDTO {
     private Long productId;
     private String productName;
-    private String image;
     private String description;
+    private String image;
     private Integer quantity;
     private Double price;
     private double discount;
-    private Double specialPrice;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
-
+    private double specialPrice;
 }
